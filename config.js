ebe583aa819452b66e6e8fe89344f415d598312a
veterans-ladevesa <veteransladevesa@gmail.com>
2026-04-22 13:01:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   SUPABASE_URL: 'https://veterans-db.supabase.co',
-  SUPABASE_ANON_KEY: 'ayvdiffhxlqeurvukyul'
+  SUPABASE_ANON_KEY: 'sb_publishable_FoVCjQel6oUguM4K1FRhvA_BokVP9FT'
 };
